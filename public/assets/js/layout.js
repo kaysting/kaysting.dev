f92c1a4a306f1289bb6d292830b7f529315dec70
Kayla Kersting <kayla@kaysting.dev>
2026-04-18 02:28:08 -0500

    Add socials to about page, make sidebar collapsing save state and work on mobile

--- public/assets/js/layout.js
+++ public/assets/js/layout.js
@@ -9,12 +9,27 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('scroll', checkScroll);
     checkScroll();
 
-    const sidebar = document.querySelector('#sidebar');
     document.querySelector('#sidebarToggle').addEventListener('click', () => {
         if (window.innerWidth > 1000) {
-            sidebar.classList.toggle('hidden');
+            const isCurrentlyHidden = document.body.classList.contains('sidebarHidden');
+            const newState = !isCurrentlyHidden;
+            window.localStorage.setItem('isSidebarHidden', newState.toString());
+            if (newState) document.body.classList.add('sidebarHidden');
+            else document.body.classList.remove('sidebarHidden');
         } else {
-            sidebar.classList.toggle('open');
+            document.body.classList.toggle('sidebarOpen');
         }
     });
+
+    document.querySelector('#sidebarDimming').addEventListener('click', () => {
+        document.body.classList.remove('sidebarOpen');
+    });
+
+    const sidebar = document.querySelector('#sidebar');
+    const sidebarButtons = sidebar.querySelectorAll('.btn');
+    for (const btn of sidebarButtons) {
+        btn.addEventListener('click', () => {
+            document.body.classList.remove('sidebarOpen');
+        });
+    }
 });
--- routes/home.js
+++ routes/home.js
@@ -12,7 +12,33 @@ router.get('/about', async (req, res) => {
         pageTitle: `About me`,
         pageSymbol: 'person',
         metaTitle: `Hi, I'm Kayla!`,
-        metaDescription: `I'm a recent college graduate and mostly self-taught full-stack web developer and system administrator. Click to learn more about me!`
+        metaDescription: `I'm a recent college graduate and mostly self-taught full-stack web developer and system administrator. Click to learn more about me!`,
+
+        socials: [
+            { symbol: 'email', label: 'kayla@kaysting.dev', href: 'mailto:kayla@kaysting.dev' },
+            { logo: 'discord-color', label: 'kaysting', href: 'https://discord.com/app' },
+            { logo: 'github', label: 'kaysting', href: 'https://github.com/kaysting' },
+            { logo: 'npm-color', label: 'cybergen49', href: 'https://www.npmjs.com/~cybergen49' },
+            { logo: 'reddit-color', label: 'cybergen49', href: 'https://discord.com/app' },
+            { logo: 'youtube-color', label: 'kaystingdev', href: 'https://youtube.com/@kaystingdev' },
+            { logo: 'instagram-color', label: 'kaystingdev', href: 'https://www.instagram.com/kaystingdev' },
+            { logo: 'spotify-color', label: 'Kayla', href: 'https://open.spotify.com/user/dbqectp3dtptpcbdjm5nz9jaa' },
+            { logo: 'osu-color', label: 'kaysting', href: 'https://osu.ppy.sh/users/22737645' },
+            { logo: 'steam-color', label: 'kaysting', href: 'https://steamcommunity.com/id/kaysting/' },
+            { logo: 'monkeytype-color', label: 'kaysting', href: 'https://monkeytype.com/profile/kaysting' },
+            {
+                logo: 'googlemaps-color',
+                label: 'Kayla',
+                href: 'https://www.google.com/maps/contrib/103985333402100090631'
+            },
+            {
+                logo: '16personalities-color',
+                label: 'Kayla (ISFJ-T)',
+                href: 'https://www.16personalities.com/profiles/35b15de7a00c8'
+            }
+        ],
+
+        projects: []
     });
 });
 
