5357cd348efd01ba5c12393fea9fcd030e2f2252
Kayla Kersting <kayla@kaysting.dev>
2026-05-23 05:25:50 -0500

    Remove stray exclamation mark

--- data/about.js
+++ data/about.js
@@ -34,7 +34,7 @@ module.exports = {
     projects: [
         {
             logo: 'osufeed.png',
-            name: '!osu!feed',
+            name: 'osu!feed',
             desc: `A live feed of newly submitted osu! scores with tons of ways to filter what you see. This builds a user-friendly UI on top of osu-score-feed.`,
             website: 'https://osufeed.kaysting.dev/',
             github: 'https://github.com/kaysting/osu-score-feed',
